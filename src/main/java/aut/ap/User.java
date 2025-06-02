032755b8a3d7b0635b34ad8a8c3af5bb8d13a5a1
goli <shirafkanzeynab63@gmail.com>
2025-06-02 21:26:40 +0330

    update User

--- src/main/java/aut/ap/User.java
+++ src/main/java/aut/ap/User.java
@@ -1,11 +1,47 @@
 package aut.ap;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CurrentTimestamp;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Column(name = "age", nullable = false)
     private Integer age;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
+    //constructors
+    public User(){
+
+    }
+
+    public User(String lastName, String firstName, Integer age, String email, String password) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+        this.email = email;
+        this.password = password;
+    }
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
 
     public String getFirstName() {
         return firstName;
