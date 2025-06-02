18b76642c8459f65243b30ef0880d35a152109e1
goli <shirafkanzeynab63@gmail.com>
2025-06-02 18:29:57 +0330

    sync database with the java code

--- src/main/java/aut/ap/Main.java
+++ src/main/java/aut/ap/Main.java
@@ -1,7 +1,8 @@
 package aut.ap;
-
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       
     }
 }
