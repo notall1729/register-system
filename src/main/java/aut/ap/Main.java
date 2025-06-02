c6d22ae4e1c4f5e60a2f7c01b7fcf25db562326c
goli <shirafkanzeynab63@gmail.com>
2025-06-02 21:11:03 +0330

    add sign up commands

--- src/main/java/aut/ap/Main.java
+++ src/main/java/aut/ap/Main.java
@@ -1,13 +1,14 @@
 package aut.ap;
+import jakarta.transaction.UserTransaction;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
             System.out.println("[L]ogin, [S]ign up");
             String command = scanner.nextLine();
@@ -29,6 +30,25 @@ public class Main {
     }
 
     public static void signUp(){
+        User user;
+        System.out.print("First name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Last name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Age: ");
+        Integer age = scanner.nextInt();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("password: ");
+        String password = scanner.nextLine();
+        if (password.length() < 8){
+            System.out.println("Weak password.");
+            signUp();
+        }
 
     }
 }
